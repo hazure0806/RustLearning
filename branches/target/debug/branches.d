8f1d1082dd6e8d2bccb1d9cee52e9292524f5d26
岩田 岳志 <iwata_takeshi@everyhomey.co.jp>
2025-08-27 09:10:10 +0900

    Ignore target directory

--- branches/target/debug/branches.d
+++
@@ -1,1 +0,0 @@
-/Users/iwatatakeshi/Projects/RustLearning/branches/target/debug/branches: /Users/iwatatakeshi/Projects/RustLearning/branches/src/main.rs
--- branches/target/debug/deps/branches-255cf6091087ef28.d
+++
@@ -1,5 +0,0 @@
-/Users/iwatatakeshi/Projects/RustLearning/branches/target/debug/deps/branches-255cf6091087ef28.d: src/main.rs
-
-/Users/iwatatakeshi/Projects/RustLearning/branches/target/debug/deps/branches-255cf6091087ef28: src/main.rs
-
-src/main.rs:
